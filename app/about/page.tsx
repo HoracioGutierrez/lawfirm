218307bd95ee77742de65786dc400a71f1816923
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-04-18 18:13:05 -0300

    Add vision and mission to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,6 +8,7 @@ function AboutPage() {
     return (
         <main className="px-6 md:px-10 lg:px-14 xl:px-20">
             <div className="max-w-[1600px] mx-auto py-10 flex flex-col gap-10 lg:gap-16 xl:gap-24">
+
                 <section className="flex flex-col gap-10 lg:gap-20">
                     <div className="flex flex-col sm:flex-row sm:items-end sm:gap-10 md:gap-20 lg:gap-28 xl:gap-36">
                         <Image src="/person-1.png" alt="person-1" width={405} height={418} className="w-full sm:w-1/2 max-w-[405px]" />
@@ -22,32 +23,57 @@ function AboutPage() {
                         <p>Entendemos que los asuntos legales pueden ser abrumadores, ya sea que se trate de un conflicto familiar, una disputa comercial, o la necesidad de asesoramiento preventivo. Es por eso que nuestro equipo de abogados altamente capacitados se dedica a escuchar atentamente sus inquietudes, comprender a fondo su situación específica y desarrollar estrategias legales sólidas que se adapten a sus necesidades únicas. Nos esforzamos por mantener una comunicación clara y abierta con usted a lo largo de todo el proceso legal, para que siempre esté informado y empoderado para tomar decisiones importantes.</p>
                     </div>
                 </section>
+
                 <section className="flex flex-col gap-20">
                     <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-8xl text-center 2xl:w-2/3 mx-auto">Abogados y consejeros profesionales al servicio</h2>
                     <div className="flex flex-col gap-14 sm:flex-row sm:justify-center">
                         <article className="flex flex-col items-center gap-12">
-                            <Image src="/lawer-1.png" alt="person-2" width={348} height={457} className="" />
+                            <div className="overflow-hidden group">
+                                <Image src="/lawer-1.png" alt="person-2" width={348} height={457} className="group-hover:scale-125 transition-transform duration-300 grayscale group-hover:grayscale-0" />
+                            </div>
                             <div className="flex flex-col items-center gap-5">
-                                <h3>Angela Estevez</h3>
+                                <h3 className="text-2xl">Angela Estevez</h3>
                                 <p className={cn(dmSans.className, "text-muted-foreground")}>Abogada Penalista</p>
                             </div>
                         </article>
                         <article className="flex flex-col items-center gap-12">
-                            <Image src="/lawer-2.png" alt="person-2" width={348} height={457} className="" />
+                            <div className="overflow-hidden group">
+                                <Image src="/lawer-2.png" alt="person-2" width={348} height={457} className="group-hover:scale-125 transition-transform duration-300 grayscale group-hover:grayscale-0" />
+                            </div>
                             <div className="flex flex-col items-center gap-5">
-                                <h3>Andrea Landriel</h3>
+                                <h3 className="text-2xl">Andrea Landriel</h3>
                                 <p className={cn(dmSans.className, "text-muted-foreground")}>Abogada Civil</p>
                             </div>
                         </article>
                         <article className="flex flex-col items-center gap-12">
-                            <Image src="/lawer-3.png" alt="person-2" width={348} height={457} className="" />
+                            <div className="overflow-hidden group">
+                                <Image src="/lawer-3.png" alt="person-2" width={348} height={457} className="group-hover:scale-125 transition-transform duration-300 grayscale group-hover:grayscale-0" />
+                            </div>
                             <div className="flex flex-col items-center gap-5">
-                                <h3>Daniel Ramirez</h3>
+                                <h3 className="text-2xl">Daniel Ramirez</h3>
                                 <p className={cn(dmSans.className, "text-muted-foreground")}>Abogado Comercial</p>
                             </div>
                         </article>
                     </div>
                 </section>
+
+                <section className="flex flex-col md:flex-row gap-10">
+                    <div className="flex max-w-[660px] gap-10">
+                        <div className="h-[1px] w-full max-w-[130px] bg-primary hidden md:block relative top-6"></div>
+                        <div className="flex flex-col gap-4">
+                            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">Nuestra Misión</h2>
+                            <p className={cn(dmSans.className, "text-muted-foreground")}>Brindar soluciones legales integrales y efectivas que empoderen a nuestros clientes a enfrentar sus desafíos legales con confianza. Nos dedicamos a la excelencia en la representación legal, ofreciendo un servicio personalizado y compasivo que atienda tanto las necesidades legales como las inquietudes personales de cada cliente.</p>
+                        </div>
+                    </div>
+                    <div className="flex max-w-[660px] gap-10">
+                        <div className="h-[1px] w-full max-w-[130px] bg-primary hidden md:block relative top-6"></div>
+                        <div className="flex flex-col gap-4">
+                            <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl">Nuestra Visión</h2>
+                            <p className={cn(dmSans.className, "text-muted-foreground")}>Ser reconocidos como un estudio jurídico líder en la comunidad, valorado por su compromiso con la integridad, la innovación y la obtención de resultados positivos para nuestros clientes. Aspiramos a construir relaciones duraderas basadas en la confianza y el respeto mutuo, convirtiéndonos en aliados de confianza para el éxito legal y personal de nuestros clientes.</p>
+                        </div>
+                    </div>
+                </section>
+
             </div>
         </main>
     )
